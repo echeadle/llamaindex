9fafb5865dee97a76e9e7b639a235b4fe16ac3ea
echeadle <echeadle@gmail.com>
2023-12-03 19:32:57 -0700

    Lesson 14 init

--- hello-world/main.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-if __name__ == '__main__':
-    load_dotenv()
-    print('Hello, world llamaIndex Course')
-    print(f"OPENAI_API_KEY is: {os.environ['OPENAI_API_KEY']}")
